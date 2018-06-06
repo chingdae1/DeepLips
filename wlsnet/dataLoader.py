6747edf6129731bafc5788ea94015354437c38ce
sang rok lee <lsrock1@naver.com>
2018-06-06 20:18:06 +0800

    edit dataloader

--- wlsnet/dataLoader.py
+++ wlsnet/dataLoader.py
@@ -54,53 +54,50 @@ to_int = dict(zip(int_list, char_list))
 
 class videoDataset(Dataset):
     def __init__(self, path, videoMaxLen, txtMaxLen):
-        self.queries = videoProcess(path, videoMaxLen)
-        self.labels = txtProcess(path, txtMaxLen)
+        self.queries = sorted(glob(os.path.join(path, '*/*/*.mp4')))
+        self.labels = sorted(glob(os.path.join(path, '*/*/*.txt')))
+
+        self.videoMaxLen = videoMaxLen
+        self.txtMaxLen = txtMaxLen
 
     def __len__(self):
-        return self.queries.size()[0]
+        return len(self.queries)
 
     def __getitem__(self, idx):
-        return self.queries[idx], self.labels[idx]
+        return videoProcess(self.queries[idx], self.videoMaxLen), txtProcess(self.labels[idx], txtMaxLen)
 
-def videoProcess(path, videoMaxLen):
-    dirs = glob(os.path.join(path, '*/*/*.mp4'))
-    dirs = sorted(dirs)
-    results = torch.zeros(len(dirs), videoMaxLen, 120, 120)
-    for index, dir in enumerate(dirs):
-        cap = cv2.VideoCapture(dir)
-        tmp = []
-        while(True):
-            # Capture frame-by-frame
-            ret, frame = cap.read()
-            if ret:
-                # Our operations on the frame come here
-                gray = cv2.resize(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), (120, 120)).reshape(1, 120, 120)
-                tmp.append(gray)
-                # Display the resulting frame
-                #imshow(gray, cmap='gray')
-            else:
-                break
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-        results[index, :len(tmp), :, :] = torch.from_numpy(np.concatenate(tmp, axis=0).reshape(1, -1, 120, 120))
+def videoProcess(dir, videoMaxLen):
+    cap = cv2.VideoCapture(dir)
+    tmp = []
+    results = torch.zeros(1, videoMaxLen, 120, 120)
+    while(True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        if ret:
+            # Our operations on the frame come here
+            gray = cv2.resize(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), (120, 120)).reshape(1, 120, 120)
+            tmp.append(gray)
+            # Display the resulting frame
+            #imshow(gray, cmap='gray')
+        else:
+            break
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    results[index, :len(tmp), :, :] = torch.from_numpy(np.concatenate(tmp, axis=0).reshape(1, -1, 120, 120))
     cap.release()
     return results
 
-def txtProcess(path, txtMaxLen):
+def txtProcess(dir, txtMaxLen):
     result = []
-    dirs = glob(os.path.join(path, '*/*/*.txt'))
-    dirs = sorted(dirs)
-    for dir in dirs:
-        with open(dir) as f:
-            tmp = [one_hot[i] for i in f.readline().split(':')[1].replace(' ', '').rstrip('\n')] + [one_hot['<eos>']]
-            
-            if len(tmp) < txtMaxLen:
-                result.append(tmp+[one_hot['<pad>'] for _ in range(txtMaxLen - len(tmp))])
-            
-            else:
-                print(tmp)
-                raise Exception('too short txt max length')
+    with open(dir) as f:
+        tmp = [one_hot[i] for i in f.readline().split(':')[1].replace(' ', '').rstrip('\n')] + [one_hot['<eos>']]
+        
+        if len(tmp) < txtMaxLen:
+            result.append(tmp+[one_hot['<pad>'] for _ in range(txtMaxLen - len(tmp))])
+        
+        else:
+            print(tmp)
+            raise Exception('too short txt max length')
     # dataLen = len(result)
     # vector = torch.zeros((dataLen, txtMaxLen))
     
